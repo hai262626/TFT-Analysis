32dcd163bb031367341f55e10eb0183450a40f87
hai262626 <11230693@st.neu.edu.vn>
2026-09-04 05:58:24 +0700

    .

--- dbt_tft/models/meta_info/intermediate/int_18_items.sql
+++ dbt_tft/models/meta_info/intermediate/int_18_items.sql
@@ -3,23 +3,24 @@
     schema='int_info'
 ) }}
 
-WITH stg_18_items AS (
+WITH raw_items AS (
+    SELECT 
+        *
+    FROM {{ref('stg_18_items')}}
+    
+),
+
+stg_18_items AS (
     SELECT 
         item_id,
         item_name_raw,
         item_is_augment,
         item_unique,
         item_category,
-        item_description,
-        icon_path,
-        item_effects,
         item_composition,
-        item_associated_traits,
-        item_incompatible_traits,
-        item_from,
         ingested_at,
         loaded_at         
-    FROM {{ref('stg_18_items')}}
+    FROM raw_items
     WHERE item_category IN ('CompleteItem', 'Artifact', 'Emblem', 'RadiantItem', 'Component')
 )
 
